a4e4bb49265002683d3e77c79e91dba8a9e27776
Bernardo Caldas <bernardocaldas@hotmail.com>
2025-12-08 15:48:10 +0000

    fix: use PostHogProvider for static export compatibility

--- src/instrumentation-client.ts
+++
@@ -1,7 +0,0 @@
-import posthog from 'posthog-js';
-
-posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-  api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://eu.i.posthog.com',
-  defaults: '2025-11-30',
-});
-
