63ee3988b1b26c767998f81421fa0bf1262cbe1b
Bernardo Caldas <bernardocaldas@hotmail.com>
2025-12-08 15:13:38 +0000

    fix: use instrumentation-client.ts for PostHog (Next.js 15.3+ recommended approach)

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -3,15 +3,10 @@ import { getMessages, getTranslations } from 'next-intl/server';
 import { notFound } from 'next/navigation';
 import { locales } from '@/i18n/routing';
 import { ElectionProvider } from '@/contexts/ElectionContext';
-import { PostHogPageView } from '@/components/PostHogProvider';
-import Script from 'next/script';
 import '../globals.css';
 import type { Metadata } from 'next';
 import fs from 'fs';
 import path from 'path';
-
-const POSTHOG_KEY = process.env.NEXT_PUBLIC_POSTHOG_KEY;
-const POSTHOG_HOST = process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://eu.i.posthog.com';
 
 // Read OG image filename at build time (versioned filename for cache busting)
 function getOgImageFilename(locale: string): string {
@@ -122,28 +117,6 @@ export default async function RootLayout({
         <meta name="twitter:image" content={ogImageUrl} />
       </head>
       <body className="antialiased" suppressHydrationWarning>
-        {POSTHOG_KEY && (
-          <Script
-            id="posthog-init"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `
-                !function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.crossOrigin="anonymous",p.async=!0,p.src=s.api_host+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset isFeatureEnabled onFeatureFlags getFeatureFlag getFeatureFlagPayload reloadFeatureFlags group updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures getActiveMatchingSurveys getSurveys onSessionId".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
-                posthog.init('${POSTHOG_KEY}', {
-                  api_host: '${POSTHOG_HOST}',
-                  person_profiles: 'identified_only',
-                  capture_pageview: true,
-                  capture_pageleave: true,
-                  autocapture: true,
-                  enable_heatmaps: true,
-                  disable_session_recording: false,
-                  capture_dead_clicks: true
-                });
-              `,
-            }}
-          />
-        )}
-        <PostHogPageView />
         <NextIntlClientProvider messages={messages} locale={locale}>
           <ElectionProvider>
             {children}
---
+++ src/instrumentation-client.ts
@@ -0,0 +1,13 @@
+import posthog from 'posthog-js';
+
+posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+  api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://eu.i.posthog.com',
+  person_profiles: 'identified_only',
+  capture_pageview: true,
+  capture_pageleave: true,
+  autocapture: true,
+  enable_heatmaps: true,
+  disable_session_recording: false,
+  capture_dead_clicks: true,
+});
+
